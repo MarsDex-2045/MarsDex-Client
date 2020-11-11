a6cd0378052fd26023edeca2148e21f4f52c81c8
benjamin.robbe <HPZR24wLogitech>
2020-11-11 10:17:15 +0100

    20201110 Benjamin Robbe init js for routing between pages

---
+++ src/assets/js/application/event-listeners.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function generateEventListeners() {
+    document.querySelector('#colonies').addEventListener('click', goToColonies);
+    document.querySelector('#map').addEventListener('click', goToMap);
+
+}
---
+++ src/assets/js/application/header.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function goToColonies(e) {
+    e.preventDefault();
+    window.location.assign("martian/colonies.html");
+}
+
+function goToMap(e) {
+    e.preventDefault();
+    window.location.assign("martian/global-map.html");
+}
