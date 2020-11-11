2cc902d927ea6a79d3a5d257dd15a7e5cfcf79d8
benjamin.robbe <HPZR24wLogitech>
2020-11-11 20:25:54 +0100

    20201110 Benjamin Robbe pop up company done, almost done with dropdowns

--- src/assets/js/application/html-generator.js
+++ src/assets/js/application/html-generator.js
@@ -10,7 +10,7 @@ function addPopUp() {
                         <li id="settings"><span class="fas fa-cog"></span><div>settings</div></li>
                         <li id="account"><span class="fas fa-user-circle"></span><div>account</div></li>
                         <li><a href="#"><span class="fas fa-info-circle"></span><div>help</div></a></li>
-                        <li><a href="martian/login.html"><span class="fas fa-sign-out-alt"></span><div>logout</div></a></li>                        
+                        <li><a href="../martian/login.html"><span class="fas fa-sign-out-alt"></span><div>logout</div></a></li>                        
                     </ul>
                 </nav>
                 <a href="#" class="close"><span class="fas fa-times"></span></a>
@@ -21,23 +21,38 @@ function addPopUp() {
             <h2><span class="fas fa-cog"></span><div>settings</div></h2>
             <div>
                 <h3><span class="fas fa-volume-down"></span><div>sound</div></h3>
-                <input type="radio" name="sound" id="soundOn" value="on" checked/>
-                <label for="soundOn">on</label>
-                <input\ttype="radio" name="sound" id="soundOff" value="off"/>
-                <label\tfor="soundOff">off</label>
+                <form>
+                    <div>
+                        <input type="radio" name="sound" id="soundOn" value="on"/>
+                        <label for="soundOn">on</label>
+                    </div>
+                   <div>
+                       <input type="radio" name="sound" id="soundOff" value="off" checked/>
+                       <label for="soundOff">off</label>
+                    </div>
+                </form>
             </div>
             <div>
                 <h3><span class="fas fa-language"></span><div>language</div></h3>
-                <input type="radio" name="language" id="english" value="english" checked="checked"/>
-                <label for="english">english</label>
-                <input type="radio" name="language" id="francais" value="francais"/>
-                <label for="francais">francais</label>
-                <input type="radio" name="language" id="nederlands" value="nederlands"/>
-                <label for="nederlands">nederlands</label>
-                <input type="radio" name="language" id="deutsch" value="deutsch"/>
-                <label for="deutsch">deutsch</label>
+                <form>
+                    <div>
+                        <input type="radio" name="language" id="english" value="english" checked="checked"/>
+                        <label for="english">english</label>
+                    </div>
+                    <div>
+                        <input type="radio" name="language" id="francais" value="francais"/>
+                        <label for="francais">francais</label>
+                    </div>
+                    <div>
+                        <input type="radio" name="language" id="nederlands" value="nederlands"/>
+                        <label for="nederlands">nederlands</label>
+                    </div>
+                    <div>
+                        <input type="radio" name="language" id="deutsch" value="deutsch"/>
+                        <label for="deutsch">deutsch</label>
+                    </div>
+                </form>                
             </div>
-            <a href="#"><span class="fas fa-user-secret"></span><div>privacy policy</div></a>
             <a href="#" class="close"><span class="fas fa-arrow-left"></span></a>
         </section>
             
@@ -47,7 +62,7 @@ function addPopUp() {
                 <label for="registerCompanyName">Company Name</label>
                 <input title="companyName" required type="text" id="registerCompanyName">
                     
-                <label for="registerEmail">E-mail</label>
+                <label for="registerEmail">Email</label>
                 <input autofocus title="email" required type="text" id="registerEmail">
 
                 <label for="registerColonyName">Colony Name</label>
@@ -62,7 +77,7 @@ function addPopUp() {
                 <label for="registerPasswordRepeat">Repeat Password</label>
                 <input title="password" required type="password" id="registerPasswordRepeat">
       
-                <input type="submit" value="save changes" title="save changes">
+                <input type="submit" value="save changes" title="save changes" id="save">
             </form>
             <a href="#" class="close"><span class="fas fa-arrow-left"></span></a>
         </section>
