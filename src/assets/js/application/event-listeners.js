10a40b986e9ca82310e3979dff4466c1313e76b2
benjamin.robbe <HPZR24wLogitech>
2020-11-11 10:21:59 +0100

    20201110 Benjamin Robbe reverted

--- src/assets/js/application/event-listeners.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function generateEventListeners() {
-    document.querySelector('#colonies').addEventListener('click', goToColonies);
-    document.querySelector('#map').addEventListener('click', goToMap);
-
-}
