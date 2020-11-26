a508978561275f07ed227dab3eed74ea592606e9
yarne.savaete <yarne.savaete@student.howest.be>
2020-11-26 17:39:55 +0100

    Removed a Sonar Issue, Too many Returns in 1 function

--- src/assets/js/colonies/colony.js
+++ src/assets/js/colonies/colony.js
@@ -35,15 +35,12 @@ function calculateLocation(colonyLocation) {
 }
 
 function locationToDegrees(location, type) {
-    console.log("LOCATION: " + location);
-
     const degrees = location.substring(0, location.indexOf("."));
     const minutesCalculation = (parseFloat("0" + location.substring(location.indexOf("."), location.length)) * 60).toString();
     const minutes = minutesCalculation.substring(0, minutesCalculation.indexOf("."));
     const seconds = Math.round(parseFloat("0" + minutesCalculation.substring(minutesCalculation.indexOf("."), minutesCalculation.length)) * 60);
 
     let locationString = "";
-
     if (type === "latitude") {
         if (degrees >= "0") { locationString = `${degrees}° ${minutes}' ${seconds}" N`; }
         else if (degrees < "0") { locationString = `${degrees}° ${minutes}' ${seconds}" S`; }
