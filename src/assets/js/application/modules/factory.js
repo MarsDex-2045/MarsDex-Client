3d0ea0101a1cb1d4d3ca1db4ef81ab323c50cede
Bo <bo.robbrecht@student.howest.be>
2020-12-16 11:18:33 +0100

    Added marker & popups to the map based on the colonies

--- src/assets/js/application/modules/factory.js
+++ src/assets/js/application/modules/factory.js
@@ -2,9 +2,20 @@ export function baseMap(target, colonies) {
     navigator.geolocation.getCurrentPosition((location) => {
         const userCoordinates = [location.coords.longitude, location.coords.latitude];
         const map = L.map(target).setView(userCoordinates, 4);
-        let baselayer = new L.tileLayer('https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-mars-basemap-v0-2/all/{z}/{x}/{y}.png', {
-            zoom: 3,
-            tms: false,
-        }).addTo(map).setZIndex(0);
+        addBaseLayer(map);
+        addColonyMarkers(colonies, map);
+    });
+}
+
+function addBaseLayer(target){
+    new L.tileLayer('https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-mars-basemap-v0-2/all/{z}/{x}/{y}.png', {
+        zoom: 3,
+        tms: false,
+    }).addTo(target).setZIndex(0);
+}
+
+function addColonyMarkers(dataset, target) {
+    dataset.forEach(colony => {
+        L.marker([colony.location.longitude, colony.location.latitude]).bindPopup(`<b>${colony.name}</b>`).addTo(target);
     });
 }
