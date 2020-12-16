a54d222e9147a358344d0cb76ea7668ef0ec2a10
Bo <bo.robbrecht@student.howest.be>
2020-12-16 16:50:28 +0100

    Able to retrieve the company's colony

--- src/assets/js/resources/all-resources.js
+++ src/assets/js/resources/all-resources.js
@@ -1,8 +1,32 @@
 "use strict";
-import {createNewResourceElement} from "./modules/factory.js";
 
 document.addEventListener("DOMContentLoaded", init);
+const companyId = 2;
+
 
 function init() {
-    console.log("Script loaded");
+    try {
+        getColonyOfCompany().then(response => {
+            console.log(response);
+        });
+    } catch (ex) {
+        sendError(ex.message);
+    }
+}
+
+function getColonyOfCompany() {
+    return getCompany(2).then(company => {
+        return getColonies().then(colonies => {
+            return crossReferenceColonyName(company, colonies);
+        });
+    });
+    function crossReferenceColonyName(needle, haystack) {
+        const res = haystack.filter(colony => colony.name.toLowerCase() === needle.colony.toLowerCase());
+        if(res === undefined){
+            throw new Error("No colony found");
+        } else{
+            return res;
+        }
+    }
+
 }
