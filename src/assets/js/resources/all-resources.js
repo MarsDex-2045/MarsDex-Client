c1e9cdc16e325af466ba2c118437553e44abf76b
Bo <bo.robbrecht@student.howest.be>
2020-12-16 19:28:48 +0100

    Able to limit displayed result. Comes included with single page checks, page counter and is usable for different maxResults

--- src/assets/js/resources/all-resources.js
+++ src/assets/js/resources/all-resources.js
@@ -3,18 +3,26 @@ import {createNewUnmodifiableResourceElement as resourceElement} from "./modules
 
 document.addEventListener("DOMContentLoaded", init);
 const companyId = 2;
+let page = 0;
+const maxResults = 6;
+let maxPages;
+let totalResources;
 
 function init() {
     try {
         getColonyOfCompany().then(response => {
-            console.log(response[0].id, response);
             getColonyDetails(response[0].id).then(details => {
-               resourceFilter(sortResourcesByAlphabet(details.resources)).forEach(resource => resourceElement(resource, "#resourcesResultList"));
+                totalResources = resourceFilter(sortResourcesByAlphabet(details.resources));
+                maxPages = Math.ceil(totalResources.length / maxResults);
+                displayResults(totalResources);
+                document.querySelector("#pageNumber").innerHTML = `${page + 1}/${maxPages}`;
             });
         });
     } catch (ex) {
         sendError(ex.message);
     }
+    document.querySelector("#nextPage").addEventListener("click", nextPage);
+    document.querySelector("#previousPage").addEventListener("click", previousPage);
 }
 
 function getColonyOfCompany() {
@@ -32,5 +40,31 @@ function getColonyOfCompany() {
             return res;
         }
     }
+}
 
+function displayResults(resources) {
+    document.querySelector("#resourcesResultList").innerHTML = "";
+    const max = (maxResults + (page * maxResults)) > totalResources.length ? totalResources.length : (maxResults + (page * maxResults));
+    const count = (page * maxResults);
+    for (let i = count ; i < max; i++) {
+        resourceElement(resources[i], "#resourcesResultList");
+    }
+}
+
+function nextPage(e){
+    e.preventDefault();
+    if(page < maxPages - 1 && totalResources.length > maxResults){
+        page++;
+        displayResults(totalResources);
+        document.querySelector("#pageNumber").innerHTML = `${page + 1}/${maxPages}`;
+    }
+}
+
+function previousPage(e){
+    e.preventDefault();
+    if(page > 0){
+        page--;
+        displayResults(totalResources);
+        document.querySelector("#pageNumber").innerHTML = `${page + 1}/${maxPages}`;
+    }
 }
