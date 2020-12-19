db34e633679d41a0972765db98d8eadd5b13c4fc
Bo <bo.robbrecht@student.howest.be>
2020-12-19 11:57:49 +0100

    Removed the else statement, seeing as it doesn't do anything in sw.js.

--- src/sw.js
+++ src/sw.js
@@ -10,10 +10,6 @@ self.addEventListener("notificationclick", e => {
             e.waitUntil(
                 self.clients.openWindow("./index.html")
             );
-        } else if (e.notification.tag === "no") {
-            e.waitUntil(
-                self.clients.openWindow("http://127.0.0.1:5501/denied.html")
-            );
         }
     }
 );
